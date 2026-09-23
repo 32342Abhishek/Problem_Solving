08795c96c87c85a00d84950a79a6d582a7c8acb7
Abhishek Kumar <157559756+32342Abhishek@users.noreply.github.com>
2026-09-23 15:21:48 +0530

    Time: 17 ms (40.72%), Space: 43.8 MB (57.98%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -5,14 +5,22 @@ class Solution {
         for(int i=0;i<s.length();i++){
           char a=s.charAt(i);
           char b=t.charAt(i);
-          if(map1.containsKey(a) && map1.get(a)!=b) {
+          if(map1.containsKey(a)) {
+            if(map1.get(a)!=b){
+        
             return false;
+            }
+          }else{
+            map1.put(a,b);
           }
-          if(map2.containsKey(b) && map2.get(b)!=a){
+          if(map2.containsKey(b)){
+            if(map2.get(b)!=a){
             return false;
-          }
-          map1.put(a,b);
+            }
+          }else{
+          
           map2.put(b,a);
+          }
 
         }
         

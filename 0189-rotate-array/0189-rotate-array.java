3ff57eb18d400b1dc6b43b1600ae9d4079815281
Abhishek Kumar <157559756+32342Abhishek@users.noreply.github.com>
2026-09-27 00:34:15 +0530

    Time: 4 ms (69.15%), Space: 268 MB (63.02%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -3,9 +3,8 @@ class Solution {
            int n=arr.length;
       d=d%n;
       reverse(arr,0,n-1);
-      
-      reverse(arr,d,n-1);
       reverse(arr,0,d-1);
+      reverse(arr,d,n-1);
         
         
     }
